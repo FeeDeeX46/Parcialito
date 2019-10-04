1ba591477dcfc24b6f370706a957c100aad25e07
FeeDeeX46 <55369229+FeeDeeX46@users.noreply.github.com>
2019-10-04 00:21:02 -0300

    Update Guitarrista.cs

--- Guitarrista.cs
+++ Guitarrista.cs
@@ -14,15 +14,12 @@ namespace Parcialito
 		
 		public void ensayar()
 		{
-			guitarra.acomodarGuitarra();
-			guitarra.revisarCuerdas();
-			guitarra.afinarCuerdas();
+			guitarra.ensayar();
 		}
 		
 		public void tocar()
 		{
-			guitarra.leerPartitura();
-			guitarra.tocarInstrumento();			
+			guitarra.tocar();		
 		}
 	}
 }
